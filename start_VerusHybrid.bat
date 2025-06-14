e84d2b043dd3d0898c61792f1ed40a50d17af742
Inulekik <inulekik89@gmail.com>
2025-06-14 15:00:22 +0700

    Add files via upload

---
+++ start_VerusHybrid.bat
@@ -0,0 +1,5 @@
+@echo off 
+cd %~dp0 
+ 
+cls 
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool cn.vipor.net:5040 --wallet RBgR672WmULsgLgjg2VCxQ9ikQphZGh6eL --Worker VerusPc --password --cpu-threads 4
