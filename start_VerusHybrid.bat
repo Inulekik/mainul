fae6c37a764f10c37c9e860925bab9f245d227c2
Inulekik <inulekik89@gmail.com>
2025-06-14 15:25:50 +0700

    Update start_VerusHybrid.bat

--- start_VerusHybrid.bat
+++ start_VerusHybrid.bat
@@ -2,4 +2,4 @@
 cd %~dp0 
  
 cls 
-SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool cn.vipor.net:5040 --wallet RBgR672WmULsgLgjg2VCxQ9ikQphZGh6eL --Worker VerusPc --password --cpu-threads 4
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool cn.vipor.net:5040 --wallet RBgR672WmULsgLgjg2VCxQ9ikQphZGh6eL --Worker VerusPc --password --cpu-threads 1
